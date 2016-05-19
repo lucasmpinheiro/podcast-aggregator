d5e3bc1a25151a2f282045ac03c09fb71529144a
LucasMP <lucasmpinheiro@gmail.com>
2016-05-19 00:09:32 -0300

    Create basic iTunes search service.

---
+++ app/services/itunes.ts
@@ -0,0 +1,16 @@
+import { Injectable }     from 'angular2/core';
+import { Http, Response } from 'angular2/http';
+
+
+@Injectable()
+export class HeroService {
+    constructor(private http: Http) { }
+
+    // Itunes search URL.
+    private itunesUrl = 'https://itunes.apple.com/search';
+
+    getPodcastsList(term: string) {
+        let queryUrl: string = this.itunesUrl + '?media=podcast&term=' + term;
+        return this.http.get(queryUrl);
+    }
+}
