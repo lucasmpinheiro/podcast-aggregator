ebdf9f1b95142ba0ece9928be4cecae763d2a1e5
LucasMP <lucasmpinheiro@gmail.com>
2016-05-30 22:47:18 -0300

    Add a feed reader service.

--- gulpfile.js
+++ gulpfile.js
@@ -35,38 +35,44 @@ var copyScripts = require('ionic-gulp-scripts-copy');
 
 var isRelease = argv.indexOf('--release') > -1;
 
-gulp.task('watch', ['clean'], function(done){
-  runSequence(
-    ['sass', 'html', 'fonts', 'scripts'],
-    function(){
-      gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
-      gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
-      buildBrowserify({ watch: true }).on('end', done);
-    }
-  );
+gulp.task('watch', ['clean'], function(done) {
+    runSequence(
+        ['sass', 'html', 'fonts', 'scripts'],
+        function() {
+            gulpWatch('app/**/*.scss', function() {
+                gulp.start('sass');
+            });
+            gulpWatch('app/**/*.html', function() {
+                gulp.start('html');
+            });
+            buildBrowserify({
+                watch: true
+            }).on('end', done);
+        }
+    );
 });
 
-gulp.task('build', ['clean'], function(done){
-  runSequence(
-    ['sass', 'html', 'fonts', 'scripts'],
-    function(){
-      buildBrowserify({
-        minify: isRelease,
-        browserifyOptions: {
-          debug: !isRelease
-        },
-        uglifyOptions: {
-          mangle: false
+gulp.task('build', ['clean'], function(done) {
+    runSequence(
+        ['sass', 'html', 'fonts', 'scripts'],
+        function() {
+            buildBrowserify({
+                minify: isRelease,
+                browserifyOptions: {
+                    debug: !isRelease
+                },
+                uglifyOptions: {
+                    mangle: false
+                }
+            }).on('end', done);
         }
-      }).on('end', done);
-    }
-  );
+    );
 });
 
 gulp.task('sass', buildSass);
 gulp.task('html', copyHTML);
 gulp.task('fonts', copyFonts);
 gulp.task('scripts', copyScripts);
-gulp.task('clean', function(){
-  return del('www/build');
+gulp.task('clean', function() {
+    return del('www/build');
 });
