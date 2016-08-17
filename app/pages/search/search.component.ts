fc32afac23591a73829f0474a79f0f06c608d1f0
LucasMP <lucasmpinheiro@gmail.com>
2016-08-16 21:55:50 -0300

    Show search results when it's submitted.

--- app/pages/search/search.component.ts
+++ app/pages/search/search.component.ts
@@ -2,18 +2,21 @@
 import {Component, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
 import {Color} from 'color';
 import {SearchBar} from 'ui/search-bar';
+import {ObservableArray} from 'data/observable-array';
 import {ItunesService} from '../../shared/itunes/itunes.service';
 
 @Component({
+    selector: "search",
     templateUrl: 'pages/search/search.html',
     providers: [ItunesService]
 })
 export class SearchPage implements AfterViewInit {
     // local: Storage;
     searchTerm: string = '';
-    results: Array<Object> = [];
-    isLoading: boolean = false;
     nsSearchBar: SearchBar;
+    results: ObservableArray<Object> = new ObservableArray();
+
+    isLoading: boolean = false;
 
     @ViewChild('searchBar') ngSearchBar: ElementRef;
 
@@ -25,71 +28,39 @@ export class SearchPage implements AfterViewInit {
         this.nsSearchBar = this.ngSearchBar.nativeElement;
         this.nsSearchBar.focus();
 
-        this.nsSearchBar.backgroundColor = new Color('black');
-        this.nsSearchBar.textFieldHintColor = new Color('gray');
-        this.nsSearchBar.color = new Color('white');
+        this.nsSearchBar.on(SearchBar.submitEvent, e => {
+            this.searchTerm = this.searchTerm.trim();
 
-        this.nsSearchBar.on(SearchBar.submitEvent, (e) => {
+            // If no term was searched for, do nothing.
+            if (this.searchTerm == '') {
+                // Clean last results.
+                this.results.splice(0);
+                return;
+            }
+
             this.isLoading = true;
 
-            console.log(this.searchTerm);
+            this._itunes.fetchPodcastsList(this.searchTerm)
+                .then(res => {
+                    // Clean last results.
+                    this.results.splice(0);
 
-            // this._itunes.fetchPodcastsList(this.searchTerm);
+                    // Add each item to the array.
+                    res.forEach(item => this.results.push(item));
 
-            this._itunes.fetchPodcastsList(this.searchTerm).then((res) => {
-                this.results = res.toJSON().results;
-                console.log(JSON.stringify(this.results[0]));
-
-                this.isLoading = false;
-            });
-
-
-            // this._itunes.fetchPodcastsList(this.searchTerm).subscribe(res => {
-            //     // this.results = res.json().results;
-            //     console.log(res);
-            //
-            //     this.isLoading = false;
-            //
-            //     // this.local.setJson('search=' + term, this.results);
-            // });
-            // this.nsSearchBar.dismissSoftInput();
+                    this.isLoading = false;
+                })
+                .catch(err => console.log(err));
         });
+
+        this.nsSearchBar.on(SearchBar.clearEvent, e => {
+            // Clean results.
+            this.results.splice(0);
+        })
 
         // this.nsSearchBar.on(SearchBar.clearEvent, (e) => {
         //     console.log('Text cleared.');
         // });
-    }
-
-    searchPodcasts(term: string) {
-        // If no term was searched for, do nothing.
-        if (term == '') {
-            this.results = [];
-            return;
-        }
-
-        // this._itunes.fetchPodcastsList(term).subscribe(res => {
-        //     // this.results = res.json().results;
-        //     console.log(res);
-        //
-        //     this.isLoading = false;
-        //
-        //     // this.local.setJson('search=' + term, this.results);
-        // });
-
-        // this.local.getJson('search=' + term).then(data => {
-        //     // If there's no local data matching the search, fetch from iTunes.
-        //     if (!data) {
-        //
-        //         return;
-        //     }
-        //     this.results = data;
-        //
-        //     // Dismiss the loading overlay.
-        //     setTimeout(()=>{
-        //         loading.dismiss();
-        //     }, 1);
-        // });
-
     }
 
     // itemTapped(event, item) {
--- app/shared/itunes/itunes.service.ts
+++ app/shared/itunes/itunes.service.ts
@@ -1,12 +1,9 @@
 import {Injectable} from '@angular/core';
 import * as http from 'http';
-import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ItunesService {
-    podcastsList: Array<Object>;
-
     constructor() { }
 
     // Itunes search URL.
@@ -15,29 +12,14 @@ export class ItunesService {
     // Search parameters.
     private searchParams: string = 'media=podcast&attribute=titleTerm&entity=podcast';
 
-    fetchPodcastsList(term: string) {
-        console.log('estou aqui');
-        let queryUrl: string =  this.itunesUrl + '?' + this.searchParams
-                                + '&term=' + term;
+    // Fetch podcasts.
+    fetchPodcastsList(term: string): Promise<any> {
+        let queryUrl: string
+            = `${this.itunesUrl}?${this.searchParams}&term=${term}`;
 
         return http.request({
             url: queryUrl,
             method: 'GET'
-        }).then((res) => {
-            return res.content;
-        }, (err) => {
-            console.error(err);
-        });
-        // return .get(queryUrl)
-        //     .map(res => res.json())
-        //     .map(data => {
-        //         console.log(JSON.stringify(data.json()));
-        //         return JSON.stringify(data.json());
-        //     });
-    }
-
-    handleErrors(error: Response) {
-        console.log(JSON.stringify(error.json()));
-        return Observable.throw(error);
+        }).then(res => res.content.toJSON().results);
     }
 }
